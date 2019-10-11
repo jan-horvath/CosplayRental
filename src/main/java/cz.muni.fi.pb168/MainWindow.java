6a6fd2bd47c2c3d26a72a9dffe40b6deda1c9d1a
Radka Sedlakova <x460676@fi.muni.cz>
2019-10-11 22:25:07 +0200

    Navigate bar showing 3 items.

--- src/main/java/cz.muni.fi.pb168/MainWindow.java
+++ src/main/java/cz.muni.fi.pb168/MainWindow.java
@@ -5,6 +5,19 @@ public class MainWindow extends JFrame {
 
     public MainWindow() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuCatalogue = new JMenu("Catalogue");
+        JMenu menuOrderCatalogue = new JMenu("Order catalogue");
+        JMenu menuForm = new JMenu("Form");
+
+        menuBar.add(menuCatalogue);
+        menuBar.add(menuOrderCatalogue);
+        menuBar.add(menuForm);
+
+        setJMenuBar(menuBar);
+        // I wanted to put an image into the main window just to entertain you, but after half an hour of
+        // finding the simple and elegant solution I gave it up, sorry. Maybe the next time. Good night!
     }
 
 
