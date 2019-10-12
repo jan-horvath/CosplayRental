37de50d2c9d97a0b72a5fd0839cda80b7e42efb3
Radka Sedlakova <x460676@fi.muni.cz>
2019-10-12 21:41:40 +0200

    Examples if cardLayout stuff. Form not implemented yet.

--- src/main/java/cz.muni.fi.pb168/MainWindow.java
+++ src/main/java/cz.muni.fi.pb168/MainWindow.java
@@ -2,11 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class MainWindow extends JFrame {
 
     public MainWindow() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setTitle("CoReS: Cosplay Rental Service ©");
 
         JMenuBar menuBar = new JMenuBar();
         JMenu menuCatalogue = new JMenu("Catalogue");
@@ -46,8 +50,58 @@ public class MainWindow extends JFrame {
 
         add(tb, BorderLayout.BEFORE_FIRST_LINE);
         pack();
-    }
 
+        //just testing stuff
+
+        JButton button1 = new JButton("Button1");
+        JButton button2 = new JButton("Button2");
+        JButton button3 = new JButton("Button3");
+
+        JPanel card1 = new JPanel();
+        card1.add(button1);
+        JPanel card2 = new JPanel();
+        card2.add(button2);
+        JPanel card3 = new JPanel();
+        card3.add(button3);
+
+        JPanel cards = new JPanel(new CardLayout());
+        cards.add(card1, "card1");
+        cards.add(card2, "card2");
+        cards.add(card3, "card3");
+        getContentPane().add(cards);
+
+        formButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) cards.getLayout();
+                cardLayout.show(cards, "card1");
+            }
+        });
+
+        catalogueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) cards.getLayout();
+                cardLayout.show(cards, "card2");
+            }
+        });
+
+        orderCatalogueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) cards.getLayout();
+                cardLayout.show(cards, "card3");
+            }
+        });
+        //TODO: resizable height but set width
+        addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent event) {
+                setSize(
+                        Math.max(Integer.MIN_VALUE, getWidth()),
+                        Math.max(700, getHeight()));
+            }
+        });
+    }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
