47b62f019ae11884487af365304ce20edba8ec60
Ján Horváth <xhorvat6@fi.muni.cz>
2019-10-12 11:26:59 +0200

    Catalog table implemented

--- src/main/java/cz.muni.fi.pb168/MainWindow.java
+++ src/main/java/cz.muni.fi.pb168/MainWindow.java
@@ -1,7 +1,17 @@
+import com.google.common.collect.ImmutableList;
+
 import javax.swing.*;
+import javax.swing.table.TableModel;
 import java.awt.*;
+import java.util.List;
 
 public class MainWindow extends JFrame {
+
+    private static final List<CatalogEntry> CATALOG_TEST_DATA = ImmutableList.of(
+            new CatalogEntry("Asterix helmet", 17.80),
+            new CatalogEntry("Poseidon trident", 21.90),
+            new CatalogEntry("Deadpool suit", 42.20)
+    );
 
     public MainWindow() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -18,6 +28,17 @@ public class MainWindow extends JFrame {
         setJMenuBar(menuBar);
         // I wanted to put an image into the main window just to entertain you, but after half an hour of
         // finding the simple and elegant solution I gave it up, sorry. Maybe the next time. Good night!
+
+        TableModel catalogTableModel = new CatalogTableModel(CATALOG_TEST_DATA);
+        JTable catalogTable = new JTable(catalogTableModel);
+        catalogTable.setRowHeight(50);
+        add(new JScrollPane(catalogTable));
+
+        JButton createOrderButton = new JButton("Create order");
+        createOrderButton.setPreferredSize(new Dimension(20, 30));
+        add(createOrderButton, BorderLayout.PAGE_END);
+
+        pack();
     }
 
 
